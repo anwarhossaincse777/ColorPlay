b6f9670f5203343ff8f507ab21fea2271d35b86e
Betabloker <72620275+Betabloker@users.noreply.github.com>
2023-07-24 15:36:44 +0600

    complete

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import ColorPlay from "./components/ColorPlay.vue";
+</script>
+
+<template>
+<ColorPlay/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ColorPlay.vue
@@ -0,0 +1,77 @@
+<script setup>
+
+
+import {computed, reactive} from "vue";
+
+let properties = reactive({
+  perspective: 100,
+  rotateX: 0,
+  rotateY: 0,
+  rotateZ: 0
+
+})
+
+
+const box=computed(()=>{
+
+  return {
+    transform: `
+          perspective(${properties.perspective}px)
+          rotateX(${properties.rotateX}deg)
+          rotateY(${properties.rotateY}deg)
+          rotateZ(${properties.rotateZ}deg)
+        `}
+
+})
+
+const reset=()=>{
+  {
+    properties.perspective=100
+        properties.rotateX= 0
+      properties.rotateY= 0
+      properties.rotateZ= 0
+  }
+}
+</script>
+
+
+<template>
+
+<!--    // Note: transform:perspective(100px) rotateX(70deg) rotateY(50deg) rotateZ(60deg)-->
+<!--    // https://css-tricks.com/how-css-perspective-works/-->
+
+    <h2>CSS3 Perspective Playground</h2>
+    <main>
+      <section class="settings">
+        <div class="settings-container">
+          <label>perspective:{{properties.perspective}}px;</label>
+          <input type="range" min="0" max="999" v-model="properties.perspective"/>
+
+          <label>rotateX:{{properties.rotateX}} deg; </label>
+          <input type="range" min="-180" max="180" v-model="properties.rotateX" />
+
+          <label>rotateY:{{properties.rotateY}} deg; </label>
+          <input type="range" min="-180" max="180" v-model="properties.rotateY" />
+
+          <label>rotateZ:{{properties.rotateZ}} deg; </label>
+          <input type="range" min="-180" max="180"  v-model="properties.rotateZ"/>
+
+          <button @click.prevent="reset" type="button">Reset</button>
+
+
+        </div>
+      </section>
+      <section class="output">
+        <div class="box-container">
+          <div class="box" :style="box"></div>
+        </div>
+      </section>
+    </main>
+
+  </template>
+
+
+
+<style scoped>
+
+</style>
